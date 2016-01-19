3e4602d6676f6d8279e0e12a98b737506261d16a
kbiandji <kbiandji@lemans11.imie.lan>
2016-01-19 16:15:18 +0100

    mise a jour de la vue 19/01/2016

---
+++ GP2I/src/view/connexion/Fenetre.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view.connexion;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author kbiandji
+ */
+public class Fenetre extends JFrame implements ActionListener {
+
+    JTextField textLogin;
+    JTextField textPassword;
+    JLabel nomDeCompte;
+    JLabel motDePasse;
+    JPanel panCentre;
+    JPanel panConnexion;
+    JButton boutonConnect;
+    JButton boutonAnnuler;
+
+    public Fenetre() {
+
+        this.panCentre = new JPanel();
+
+        //ajout du bouton de connexion
+        this.boutonConnect = new JButton("Connexion");
+        this.boutonAnnuler = new JButton("Annuler");
+
+        //application d'un border layout au content pane
+        this.getContentPane().setLayout(new BorderLayout());
+
+        //identification
+        this.panConnexion = new JPanel();
+        panConnexion.setBackground(Color.white);
+        panConnexion.setPreferredSize(new Dimension(340, 140));
+        panConnexion.setBorder(BorderFactory.createTitledBorder("Identification"));
+
+        //creation  des champs de text
+        this.textLogin = new JTextField(15);
+        this.textPassword = new JTextField(15);
+
+        this.textLogin.addActionListener(this);
+        this.nomDeCompte = new JLabel("Nom de compte : ");
+        this.textPassword.addActionListener(this);
+        this.motDePasse = new JLabel("Mot de passe :  ");
+
+        this.panConnexion.add(this.nomDeCompte);
+        this.panConnexion.add(this.textLogin);
+        this.panConnexion.add(this.motDePasse);
+        this.panConnexion.add(this.textPassword);
+
+        this.panConnexion.add(boutonConnect);
+        this.panConnexion.add(boutonAnnuler);
+        this.panCentre.add(panConnexion);
+
+        this.getContentPane().add(this.panCentre, BorderLayout.CENTER);
+
+        //Création d'une fenêtre
+        this.setSize(360, 200);
+        this.setLocationRelativeTo(null);
+        this.setTitle("GP2i");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        this.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+
+    }
+}
